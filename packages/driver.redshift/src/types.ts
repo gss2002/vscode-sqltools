775222ea4ea806a8c0f53b30468b34080b97d283
gss2002 <greg@senia.org>
2025-04-10 19:53:00 -0400

    fix to allow rolearn lookup

--- packages/driver.redshift/src/types.ts
+++ packages/driver.redshift/src/types.ts
@@ -2,6 +2,7 @@ import { IConnection } from '@sqltools/types';
 
 export interface IRedshiftConnection extends IConnection {
   roleArn?: string; // Optional, as it can be provided via dialog
+  roleLookup?: string; // Optional, as it can be provided via dialog
   clusterIdentifier: string;
   database: string;
   region: string;
